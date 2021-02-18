ea083b841bfc1ab8316792e2f4eac536d2335c0b
RafalKomorowski <rafal.komorowski1@gmail.com>
2021-02-18 21:59:09 +0100

    Customer class added, SalesOrder updated

---
+++ OrdersManagementProject.Module/BusinessObjects/Base/PersonBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Base
+{
+    /// <summary>
+    /// Class that extends the DevExpress Person class.
+    /// </summary>
+    public class PersonBase : DevExpress.Persistent.BaseImpl.Person
+    {
+        public PersonBase(DevExpress.Xpo.Session session) : base(session)
+        {
+        }
+    }
+}
--- OrdersManagementProject.Module/BusinessObjects/SalesOrders/SalesOrder.cs
+++ OrdersManagementProject.Module/BusinessObjects/SalesOrders/SalesOrder.cs
@@ -1,7 +1,12 @@
 ﻿using System.ComponentModel;
+using System.Linq;
+using DevExpress.ExpressApp.Model;
 using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
 using DevExpress.Xpo;
 using Domain.Base;
+using Domain.Customers;
+using Domain.Products;
 
 namespace Domain.SalesOrders
 {
@@ -11,12 +16,15 @@ namespace Domain.SalesOrders
     {
         public SalesOrder(Session session) : base(session)
         {
+            this.Products.ListChanged += Products_ListChanged;
         }
 
         #region OrderCode
 
         private string _orderCode;
 
+        [RuleRequiredField]
+        [RuleUniqueValue]
         [Size(30)]
         public string OrderCode
         {
@@ -25,5 +33,80 @@ namespace Domain.SalesOrders
         }
 
         #endregion
+
+        #region Customer
+
+        private Customer _customer;
+
+        [RuleRequiredField]
+        public Customer Customer
+        {
+            get { return _customer; }
+            set { SetPropertyValue<Customer>(nameof(Customer), ref _customer, value); }
+        }
+
+        #endregion
+
+        #region OrderPrice
+
+        private decimal _orderPrice;
+
+        [ModelDefault("AllowEdit", "false")]
+        public decimal OrderPrice
+        {
+            get { return _orderPrice; }
+            set { SetPropertyValue<decimal>(nameof(OrderPrice), ref _orderPrice, value); }
+        }
+
+        #endregion
+
+        #region Status
+
+        private OrderStatus _status;
+
+        public OrderStatus Status
+        {
+            get { return _status; }
+            set { SetPropertyValue<OrderStatus>(nameof(Status), ref _status, value); }
+        }
+
+        #endregion
+
+        #region Products
+
+        [Association]
+        public XPCollection<Product> Products
+        {
+            get { return GetCollection<Product>(nameof(Products)); }
+        }
+
+        #endregion
+
+        #region Products_ListChanged
+
+        /// <summary>
+        /// Updates the order price when products list changed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Products_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            this.OrderPrice = this.Products.Sum(p => p.Price);
+        }
+
+        #endregion
+    }
+
+    public enum OrderStatus
+    {
+        NewOrder = 0,
+        Paid = 1,
+        Packed = 2,
+        ReadyToShip = 3,
+        Invoiced = 4,
+        Shipped = 5,
+        OnHold = 6,
+        Delivered = 7,
+        Cancelled = 8
     }
 }
