f0f2bcfe683c0bea988a790bd02ab5ab3768570a
RafalKomorowski <rafal.komorowski1@gmail.com>
2021-03-03 16:02:55 +0100

    Customer detail view updated

--- OrdersManagementProject.Module/BusinessObjects/Customers/Customer.cs
+++ OrdersManagementProject.Module/BusinessObjects/Customers/Customer.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DevExpress.Persistent.Base;
 using DevExpress.Xpo;
 using Domain.Base;
 
 namespace Domain.Customers
 {
+    [NavigationItem("Customers")]
     public class Customer : PersonBase
     {
         public Customer(Session session) : base(session)
